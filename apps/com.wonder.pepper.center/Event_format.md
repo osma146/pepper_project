0147fe36d754154ee2c5c57445f1fb6fd66fedde
osma146 <jeffhe0906@gmail.com>
2025-09-03 10:49:59 +1000

    update

--- apps/com.wonder.pepper.center/Event_format.md
+++ apps/com.wonder.pepper.center/Event_format.md
@@ -2,4 +2,15 @@
 
 ## Event base JSON
 
-{"1":{"type":"command","command_type":"python","path":"/home/nao/.local/share/PackageManager/apps/home11/ars/Sound_Module.py"},"2":{"type":"page_move","location":"home","destination":"clcik_AI"}}
+    {"1":{"type":"command","command_type":"python","path":"/home/nao/.local/share/PackageManager/apps/home11/ars/Sound_Module.py"},"2":{"type":"page_move","location":"home","destination":"clcik_AI"}}
+
+## Event type
+
+### Index
+
+    command
+    page_move
+    tag
+    say
+
+###
--- apps/com.wonder.pepper.center/ars/event_handler.py
+++ apps/com.wonder.pepper.center/ars/event_handler.py
@@ -14,3 +14,12 @@ def move_to_app(data):
         version = random.randint(1, 9999)
         url = 'http://198.18.0.1/apps/home11/{}.html?version={}'.format(value.strip(), version)
         os.system('qicli call ALTabletService.showWebview "{}"'.format(url))
+
+def run_tag(tag):
+    os.system('python /home/nao/.local/share/PackageManager/apps/home11/ars/tag.py --tag {}'.format(tag))
+    pass
+
+def command(data):
+    threading.Thread(target=lambda: os.system('{} {}'.format(data.get("command_type", ""), data.get("path", "")))).start()
+    time.sleep(10)  # Wait for command to finish before allowing new commands
+
